38fd5b7df5f676312c51ec0334a86a2e2f206a18
Nvutu-Eladio <nveladio@gmail.com>
2026-03-14 21:40:50 -0300

    config: criando a classe de configuracao do amazon Sns

---
+++ src/main/java/com/nveladio/notificacao/config/AmazonSnsConfiguration.java
@@ -0,0 +1,15 @@
+package com.nveladio.notificacao.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonSnsConfiguration {
+
+    @Value("${aws.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+}
