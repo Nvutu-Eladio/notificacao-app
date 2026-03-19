42c5ec7cc3321a62155dd85f0020b35a70f4c04d
Nvutu-Eladio <nveladio@gmail.com>
2026-03-18 21:38:34 -0300

    feat: concluindo servico de notificacao

--- src/main/java/com/nveladio/notificacao/service/NotificacaoSnsService.java
+++ src/main/java/com/nveladio/notificacao/service/NotificacaoSnsService.java
@@ -1,11 +1,23 @@
 package com.nveladio.notificacao.service;
 
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
 
 @Service
 public class NotificacaoSnsService {
 
-    public void notificar(String mensagem){
+    @Autowired
+    private SnsClient amazonSNS;
+
+    public void notificar(String telefone, String mensagem) {
+        PublishRequest publishRequest =PublishRequest.builder()
+                .message(mensagem)
+                .phoneNumber(telefone)
+                .build();
+        amazonSNS.publish(publishRequest);
 
     }
 
